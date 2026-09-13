a34f4e74b4f6ab8563c92a56fb51cfa6111335b1
angelmicelti <angelmicelti@users.noreply.github.com>
2026-09-14 01:04:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker para Evaluación Criterial Multimateria
 
-const CACHE_NAME = 'eval-criterial-multimateria-v2.0.4';
+const CACHE_NAME = 'eval-criterial-multimateria-v2.0.5';
 const BASE_PATH = '/CalificaActividadesMateriaGeneralV2/';
 const URLS_TO_CACHE = [
     BASE_PATH,
